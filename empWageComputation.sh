0c2f35c739e1266b0b1367558ee6ff7d66a192ad
301prateek <prateek.patil301@gmail.com>
2020-10-16 01:45:38 +0530

    Added: Formula to Calculate daily wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -10,11 +10,23 @@ empAttendance=$((RANDOM%2))
 empPresent=1
 empAbsent=0
 
+#To calculate Daily employee wage that is per hour is rupees 20 and for 8 hours
+
+fullDayHr=8
+
 #If contion to check if Employee is  present or absent
+
 if [ $empAttendance -eq $empPresent ]
 then
    echo "Employee is Present"
+   wagePerHr=20
 else
    echo "Employee is Absent"
+   wagePerHr=0
 fi
+
+#Calculating daily salary
+
+salary=$(( $wagePerHr * $fullDayHr ))
+echo "Todays Salary = $salary"
 
