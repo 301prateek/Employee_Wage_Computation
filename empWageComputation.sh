4c1bccf737b7e08f3d0f3aed5d4148859c2e21b8
301prateek <prateek.patil301@gmail.com>
2020-10-16 01:28:40 +0530

    Added: Condition to check attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,20 @@
 #!/bin/bash -x
+#Author: Prateek
+#This is a program to check Employee attendance
 
+#Welcome Message
 echo "Welcome to Employee Wage Computation Program"
+
+#Using Random function to generate values 0 and 1
+empAttendance=$((RANDOM%2))
+empPresent=1
+empAbsent=0
+
+#If contion to check if Employee is  present or absent
+if [ $empAttendance -eq $empPresent ]
+then
+   echo "Employee is Present"
+else
+   echo "Employee is Absent"
+fi
+
